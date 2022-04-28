388e09c50e1e8b29cf3f16afb0bdd723079d7f00
Denis_Lesnyh <Denis_Lesnyh@outlook.com>
2022-04-28 14:09:17 +0300

    Готовое

--- Final task/guess-number-task/game_final_task.py
+++ Final task/guess-number-task/game_final_task.py
@@ -1,13 +1,7 @@
-"""Игра угадай число
-Компьютер сам загадывает и сам угадывает число
-"""
-
-import numpy as np
-
-
 def optimal_predict(number: int = 1) -> int:
     '''Игра компьютер угадает число меньше чем за 20 попыток'''
 
+    import numpy as np
 
     min = 1
     max = 101
@@ -27,32 +21,26 @@ def optimal_predict(number: int = 1) -> int:
           min = mid
 
         else:
-            print(f"Компьютер угадал число за {count} попыток. Это число {number}")
             break #конец игры выход из цикла
     return count
 
-
 def score_game(optimal_predict) -> int:
-    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+  import numpy as np
+  """ За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        optimal_predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
     """
-    count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.optimal_predict(1, 101, size=(1000))  # загадали список чисел
+  count_ls = []
+  np.random.seed(1)  # фиксируем сид для воспроизводимости
+  random_array = np.random.randint(1, 101, size=(1000))  # загадали списоконлайн переводчик чисел
 
-    for number in random_array:
-        count_ls.append(optimal_predict(number))
+  for number in random_array:
+    count_ls.append(optimal_predict(number))
 
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
-    return score
-
-
-if __name__ == "__main__":
-    # RUN
-    score_game(optimal_predict)
+  print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+score_game(optimal_predict)
